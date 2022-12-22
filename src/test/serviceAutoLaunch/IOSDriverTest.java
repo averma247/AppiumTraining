b9825222878ceb4590d4a4dd775582cebc57017e
averma247 <kash.ajayverma@gmail.com>
2022-12-22 15:41:39 +0530

    ios changes. added code for ios element handling

--- src/test/serviceAutoLaunch/IOSDriverTest.java
+++ src/test/serviceAutoLaunch/IOSDriverTest.java
@@ -4,6 +4,8 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -13,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class IOSDriverTest {
     IOSDriver<IOSElement> iosdriver;
     WebDriverWait wait;
-    @Test
+    @BeforeTest
     public void initIOSDriver() throws MalformedURLException, InterruptedException {
         DesiredCapabilities cap = new DesiredCapabilities();
         System.out.println("Setting desired capabilities  -------------------------");
@@ -21,7 +23,7 @@ public class IOSDriverTest {
         cap.setCapability("deviceName","iPhone 14");
         cap.setCapability("platformVersion","16.2");
         cap.setCapability("automationName","XCUITest");
-        cap.setCapability("browserName","Safari");
+        cap.setCapability("app","/Users/ajayverma/UIKitCatalog.app");
 
         iosdriver = new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723"),cap);
         Thread.sleep(10000);
@@ -30,6 +32,23 @@ public class IOSDriverTest {
         iosdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(iosdriver,10);
 
+    }
+
+    @Test
+    public void inspectIOSElements(){
+        iosdriver.findElementByAccessibilityId("Alert Views").click();
+        iosdriver.findElementByXPath("//XCUIElementTypeStaticText[@name=\"Text Entry\"]").click();
+        iosdriver.findElementByXPath("//XCUIElementTypeTextField").sendKeys("Hello World");
+        iosdriver.findElementByAccessibilityId("OK").click();
+        iosdriver.findElementByAccessibilityId("Confirm / Cancel").click();
+        iosdriver.findElementByXPath("//XCUIElementTypeButton[@name=\"Confirm\"]").click();
+
+    }
+
+    @AfterTest
+    public void tearDown(){
+
+        iosdriver.quit();
     }
 
 }
